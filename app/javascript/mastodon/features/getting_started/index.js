de857c1bc83674fd3b7fd527c014a9c1b3154af1
yanakend <yanakend@gmail.com>
2017-09-30 04:00:21 +0900

    Adjust the size of the banner depend on height

--- app/javascript/mastodon/features/getting_started/index.js
+++ app/javascript/mastodon/features/getting_started/index.js
@@ -103,7 +103,7 @@ export default class GettingStarted extends ImmutablePureComponent {
               />
             </p>
             <a target="_blank" href="https://www.amazon.co.jp/Amazon-10-000-Amazon%E3%82%B3%E3%82%A4%E3%83%B3/dp/B00KQVXDW0?&amp;_encoding=UTF8&amp;tag=kanegongongon-22&amp;linkCode=ur2&amp;linkId=00aa68aaca62574c742c444789352dbd&amp;camp=247&amp;creative=1211">
-              <img className="amazon-banner" src="/amazon_banner.jpg" />
+              <div className="amazon-banner"></div>
             </a>
           </div>
         </div>
